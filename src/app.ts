51e7f41a876158fac2e6bbff6deeaff1dc898987
tlikits <thanchanok.likits@gmail.com>
2021-07-13 21:09:01 +0700

    init

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+import * as path from 'path'
+const app: Application = express()
+const port = 5000
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log('====================')
+  console.log('Incoming Request')
+  console.log('====================')
+  next()
+})
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.sendFile(path.join(__dirname, 'sample/calendar.ics'))
+})
+
+app.get('/calendar/*', (req: Request, res: Response, next: NextFunction) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+  console.log('Serving Calendar.ICS over URL:', fullUrl)
+  console.log('Headers:', req.headers)
+  res.sendFile(path.join(__dirname, 'sample/calendar.ics'))
+})
+
+app.listen(port, () => console.log(`Server is listening on port ${port}!`))
